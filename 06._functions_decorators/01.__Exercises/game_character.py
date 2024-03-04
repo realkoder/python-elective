d4549148ca2f2474eebf9d0abb3347fa5e2eb7a6
realkoder <alex8970@stud.kea.dk>
2024-03-04 11:18:46 +0100

    Added mopre decorators for 06 01 exercises game_character

--- 06._functions_decorators/01.__Exercises/game_character.py
+++ 06._functions_decorators/01.__Exercises/game_character.py
@@ -1,17 +1,36 @@
 
+def add_comma_space(func):
+    def wrapper():
+        return "".join([func(), ", "])
+    return wrapper
+
+def add_and_space(func):
+    def wrapper():
+        return "".join([func(), " and "])
+    return wrapper
+
+def add_punctum(func):
+    def wrapper():
+        return "".join([func(), "."])
+    return wrapper
+
 def sharpshooter(func):
     def wrapper():
-        return "".join([func(), ", the sharpshooter"])
+        return "".join([func(), "the sharpshooter"])
 
     return wrapper
 
 def stealthy(func):
     def wrapper(): 
-        return "".join([func(), " and stealthy character."])
+        return "".join([func(), "stealthy character"])
     return wrapper
 
+
+@add_punctum
 @stealthy
+@add_and_space
 @sharpshooter
+@add_comma_space
 def player():
     return "I'm the player character"
 
